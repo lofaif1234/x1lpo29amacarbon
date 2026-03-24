17c3d967fae917d2d6a137aba84fd9882eac03a9
silber <silber@carbonstudios.dev>
2026-03-24 12:11:16 +0100

    DAWIDOAWDMAWS

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,1 @@
-const _0xP_483g = [];const _0xSD_483g = function(i){let s = atob(_0xP_483g[i]);let r = '';for(let j=0; j<s.length; j++){r += String.fromCharCode(s.charCodeAt(j) ^ 25);}return r;};const _0xSD = _0xSD_483g;;
+const _0xP_5r0w = [];const _0xSD_5r0w = function(i){let s = atob(_0xP_5r0w[i]);let r = '';for(let j=0; j<s.length; j++){r += String.fromCharCode(s.charCodeAt(j) ^ 95);}return r;};const _0xSD = _0xSD_5r0w;;
