4192283b38ed6ed31a3153f3b607d6ba64ce67af
silber <silber@example.com>
2026-03-24 19:12:34 +0100

    DWINADIANDA

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const DATA_FILE = path.join(__dirname, 'data.json');
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const GITHUB_REPO_OWNER = 'melissaxe4-droid';
 const GITHUB_REPO_NAME = 'carbonstudios';
-const LICENSE_HASH = "478911e5bbda60b20801e0a9e6ecac44f7a173405f8ca926c35ca4ab4ed98b08";
+const LICENSE_HASH = "b10b7769dfe6c5eaa5862ea22bee59a81a081ca97a0a7d3bee195f4e541f4428";
 function adminAuth(req, res, next) {
     const auth = req.headers['authorization'];
     if (auth === LICENSE_HASH) {
